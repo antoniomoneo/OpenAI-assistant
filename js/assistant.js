0c2dfea22628307f1e27a3105a0c50650ecbfb38
Antonio Moneo <antoniomoneo@users.noreply.github.com>
2025-07-26 04:03:50 +0200

    Remove AMP iframe and update version

--- js/assistant.js
+++ js/assistant.js
@@ -1,2 +1,2 @@
-// Admin JS for OpenAI Assistant v2.9.23
-jQuery(()=>console.log('OA Admin loaded v2.9.23'));
+// Admin JS for OpenAI Assistant v2.9.24
+jQuery(()=>console.log('OA Admin loaded v2.9.24'));
